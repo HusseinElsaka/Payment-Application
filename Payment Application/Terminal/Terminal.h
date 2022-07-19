d4b5576cfe9046590187afb2303a4738197d0edb
HusseinElsaka <52578649+HusseinElsaka@users.noreply.github.com>
2022-07-19 12:49:59 +0200

    VS files

--- Payment Application/Terminal/Terminal.h
+++ Payment Application/Terminal/Terminal.h
@@ -21,7 +21,7 @@ typedef enum EN_terminalError_t
 }EN_terminalError_t;
 
 EN_terminalError_t getTransactionDate(ST_terminalData_t* termData);
-EN_terminalError_t isCardExpired(ST_cardData_t cardData, ST_terminalData_t termData);
+EN_terminalError_t isCardExpired(ST_cardData_t cardData, ST_terminalData_t  termData);
 EN_terminalError_t isValidCardPAN(ST_cardData_t* cardData);
 EN_terminalError_t getTransactionAmount(ST_terminalData_t* termData);
 EN_terminalError_t isBelowMaxAmount(ST_terminalData_t* termData);
