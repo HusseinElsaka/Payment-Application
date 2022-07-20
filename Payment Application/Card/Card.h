dec4d693f90ddbe95398cc8daaa5c83dc674a01c
HusseinElsaka <52578649+HusseinElsaka@users.noreply.github.com>
2022-07-20 23:18:41 +0200

    Card Files
    Adding comments to Header files

--- Payment Application/Card/Card.h
+++ Payment Application/Card/Card.h
@@ -33,15 +33,25 @@ typedef enum DateError
 	OK_DATE, WRONG_MONTH, WRONG_DAY
 }DateError;
 
-/* ask for the cardholder's name and store it into card data and Validate it */
+/* This function will ask for the cardholder's name and store it into card data.
+Card holder name is 24 characters string max and 20 min.
+If the cardholder name is NULL, less than 20 characters or more than 24 will return WRONG_NAME error, else return OK. */
 EN_cardError_t getCardHolderName(ST_cardData_t* cardData);
-/* ask for the card expiry date and store it in card data and validate it */
+
+/* This function will ask for the card expiry date and store it in card data.
+Card expiry date is 5 characters string in the format "MM/YY", e.g "05/25".
+If the card expiry date is NULL, less or more than 5 characters, or has the wrong format will return WRONG_EXP_DATE error, else return OK. */
 EN_cardError_t getCardExpiryDate(ST_cardData_t* cardData);
-/* ask for the card's Primary Account Number and store it in card data and validate it */
+
+/* This function will ask for the card's Primary Account Number and store it in card data.
+PAN is 20 characters alphanumeric only string 19 character max, and 16 character min.
+If the PAN is NULL, less than 16 or more than 19 characters, will return WRONG_PAN error, else return OK. */
 EN_cardError_t getCardPAN(ST_cardData_t* cardData);
-/* Validate Month 1 -> 12 
-  Validate Day 1 -> 30 */
+
+/* Validate Day 1 -> 30 */
 DateError checkDay(int day);
+
+/* Validate Month 1 -> 12  */
 DateError checkMonth(int month);
 
 
