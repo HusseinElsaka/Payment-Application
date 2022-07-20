de7fdc709da8335b61f29b01512efe32607d2b67
HusseinElsaka <52578649+HusseinElsaka@users.noreply.github.com>
2022-07-20 14:15:43 +0200

    CARD Functions

--- Payment Application/Card/Card.c
+++ Payment Application/Card/Card.c
@@ -20,32 +20,38 @@ DateError checkMonth(int month)
 	}
 	return OK_DATE;
 }
+
 EN_cardError_t getCardHolderName(ST_cardData_t* cardData)
 {
-	printf("Enter the CardHolder's name : \n");
+	printf("Enter the CardHolder's name : ");
 	gets(cardData->cardHolderName);
 	if ((strlen(cardData->cardHolderName) > NAME_MAX) || (strlen(cardData->cardHolderName) < NAME_MIN))
 	{
+		printf("WRONG NAME \n");
 		return WRONG_NAME;
 	}
 	return OK_CARD;
 }
+
 EN_cardError_t getCardExpiryDate(ST_cardData_t* cardData)
 {
 	// Input is in format MM/YY
-	printf("Enter the Card Expiry date : \n");
+	printf("Enter the Card Expiry date : ");
 	gets(cardData->cardExpirationDate);
 	int month = ((cardData->cardExpirationDate[0] - '0') * 10 + (cardData->cardExpirationDate[1] - '0'));
 	if (strlen(cardData->cardExpirationDate) != EXPIRE_LEN)
 	{
+		printf("WRONG EXP DATE\n");
 		return WRONG_EXP_DATE;
 	}
 	else if (checkMonth(month))
 	{
+		printf("WRONG EXP DATE\n");
 		return WRONG_EXP_DATE;
 	}
 	else if (cardData->cardExpirationDate[2] != '/')
 	{
+		printf("WRONG EXP DATE\n");
 		return WRONG_EXP_DATE;
 	}
 	return OK_CARD;
@@ -53,11 +59,12 @@ EN_cardError_t getCardExpiryDate(ST_cardData_t* cardData)
 
 EN_cardError_t getCardPAN(ST_cardData_t* cardData)
 {
-	printf("Enter Primary Account Number : \n");
+	printf("Enter Primary Account Number : ");
 	gets(cardData->primaryAccountNumber);
 	int lengthPAN = strlen(cardData->primaryAccountNumber), i;
 	if ((lengthPAN > CARD_MAX) || (lengthPAN < CARD_MIN))
 	{
+		printf("WRONG PAN \n");
 		return WRONG_PAN;
 	}
 	
@@ -65,6 +72,7 @@ EN_cardError_t getCardPAN(ST_cardData_t* cardData)
 	{
 		if (!(cardData->primaryAccountNumber[i] >= '0' && cardData->primaryAccountNumber[i] <= '9'))
 		{
+			printf("WRONG PAN \n");
 			return WRONG_PAN;
 		}
 	}
