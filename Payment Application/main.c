24e9a14e29c573a36aedbcd2e2a8ca308f5d6585
HusseinElsaka <52578649+HusseinElsaka@users.noreply.github.com>
2022-07-20 14:15:31 +0200

    main Program

--- Payment Application/main.c
+++ Payment Application/main.c
@@ -1,34 +1,12 @@
 
-#include "Server/Server.h"
+#include "Application/Application.h"
 
-/* Using this website https://www.dcode.fr/luhn-algorithm to generate Luhn Numbers */
+
 
 
 
 int main()
 {
-	ST_cardData_t TestCard;
-	ST_cardData_t *PTR_TestCard = &TestCard;
-	ST_terminalData_t TestTerminal;
-	ST_terminalData_t *PTR_TestTerminal = &TestTerminal;
-	/* CARD Data Test 
-	if ((getCardHolderName(PTR_TestCard)) || (getCardExpiryDate(PTR_TestCard)) || (getCardPAN(PTR_TestCard)))
-	{	
-		printf("WRONG Card Data \n");
-		return 1;
-	}
-	printf("Vaild Card Data \n");
-	return 0;*/
-
-	/*Terminal Data Test 
-	if (getTransactionDate(PTR_TestTerminal) || ||setMaxAmount(PTR_TestTerminal) ||  getTransactionAmount(PTR_TestTerminal) || isBelowMaxAmount(PTR_TestTerminal) ||  isValidCardPAN(PTR_TestCard))
-	{
-		printf("Wrong in  Terminal\n");
-			return 1;
-	}
-	*/
-	
-	printf("Valid Card");
-	
+	appStart();
 	return 0;
 }
