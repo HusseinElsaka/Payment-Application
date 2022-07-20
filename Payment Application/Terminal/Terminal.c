8220c5eefb340c06051d48214776434b86af7ea5
HusseinElsaka <52578649+HusseinElsaka@users.noreply.github.com>
2022-07-20 14:15:08 +0200

    Terminal Function

--- Payment Application/Terminal/Terminal.c
+++ Payment Application/Terminal/Terminal.c
@@ -9,14 +9,17 @@ EN_terminalError_t getTransactionDate(ST_terminalData_t* termData)
 	int day = ((int)termData->transactionDate[0] - '0') * 10 + ((int)termData->transactionDate[1] - '0');
 	if (strlen(termData->transactionDate) != TRANSACTION_LEN)
 	{
+		printf("WRONG DATE \n");
 		return WRONG_DATE;
 	}
 	else if (checkDay(day) || checkMonth(month))
 	{
+		printf("WRONG DATE \n");
 		return WRONG_DATE;
 	}
-	else if (termData->transactionDate[2] != '/' && termData->transactionDate[5] != '/')
+	else if (termData->transactionDate[2] != '/' || termData->transactionDate[5] != '/')
 	{
+		printf("WRONG DATE \n");
 		return WRONG_DATE;
 	}
 	return OK_TERMINAL;
@@ -31,6 +34,7 @@ EN_terminalError_t isCardExpired(ST_cardData_t cardData, ST_terminalData_t termD
 	int YearNow = (termData.transactionDate[8] - '0') * 10 + (termData.transactionDate[9] - '0');
 	if (YearExp < YearNow)
 	{
+		printf("EXPIRED CARD \n");
 		return EXPIRED_CARD;
 	}
 	else
@@ -39,6 +43,7 @@ EN_terminalError_t isCardExpired(ST_cardData_t cardData, ST_terminalData_t termD
 		{
 			if (MonthExp < MonthNow)
 			{
+				printf("EXPIRED CARD \n");
 				return EXPIRED_CARD;
 			}
 			else
@@ -58,12 +63,11 @@ EN_terminalError_t isCardExpired(ST_cardData_t cardData, ST_terminalData_t termD
 
 EN_terminalError_t getTransactionAmount(ST_terminalData_t* termData)
 {
-	printf("Enter transaction amount : \n");
+	printf("Enter Transaction amount : ");
 	scanf_s("%f", &termData->transAmount);
-
 	if (termData->transAmount <= 0)
 	{
-		printf("hi\n");
+		printf("INVALID AMOUNT\n");
 		return INVALID_AMOUNT;
 	}
 	return OK_TERMINAL;
@@ -76,7 +80,7 @@ EN_terminalError_t isValidCardPAN(ST_cardData_t* cardData)
 	long long int PAN = 0;
 	for (i = 0; i < lengthPAN; i++)
 	{
-		PAN += (long long)(cardData->primaryAccountNumber[i] - '0') * (long long)pow(10.0, lengthPAN - i - 1);
+		PAN += (long long int)(cardData->primaryAccountNumber[i] - '0') * (long long int)pow(10.0, (lengthPAN - i - 1));
 	}
 	/*Luhn Algorithm */
 	long digitPosition = 1, sum = 0;
@@ -106,9 +110,9 @@ EN_terminalError_t isValidCardPAN(ST_cardData_t* cardData)
 	}
 	if (sum % 10 == 0)
 	{
-		
 		return OK_TERMINAL;
 	}
+	printf("INVALID CARD\n");
 	return INVALID_CARD;
 }
 
@@ -119,6 +123,7 @@ EN_terminalError_t isBelowMaxAmount(ST_terminalData_t* termData)
 {
 	if (termData->transAmount > termData->maxTransAmount)
 	{
+		printf("EXCEED MAX AMOUNT\n");
 		return EXCEED_MAX_AMOUNT;
 	}
 	return OK_TERMINAL;
@@ -127,10 +132,11 @@ EN_terminalError_t isBelowMaxAmount(ST_terminalData_t* termData)
 
 EN_terminalError_t setMaxAmount(ST_terminalData_t* termData)
 {
-	printf("Enter MAX amount : \n");
+	printf("Enter MAX amount : ");
 	scanf_s("%f", &termData->maxTransAmount);
 	if (termData->maxTransAmount <= 0)
 	{
+		printf("INVALID MAX AMOUNT\n");
 		return INVALID_MAX_AMOUNT;
 	}
 	return OK_TERMINAL;
